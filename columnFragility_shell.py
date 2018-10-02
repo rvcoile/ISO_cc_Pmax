becab271870eddf10c590ee73be6a455f66524da
Ruben Van Coile <Ruben.VanCoile@UGent.be>
2018-10-02 12:10:16 +0200

    introduce *.in file modification for geometric imperfection

--- columnFragility_shell.py
+++ columnFragility_shell.py
@@ -38,7 +38,7 @@ from modSAFIR import mod_inSAFIR_multifile
 ## FUNCTION ##
 ##############
 
-def multi_Fmax(df,reffile,SW_removeIterations=True):
+def multi_Fmax(df,reffile,SW_removeIterations=True,SW_geomImperf=True):
 	## local custom code for f[Fmax_SAFIR] on multiple realizations
 	# df: pd.DataFrame : realizations for which Fmax will be calculated
 		# dimension variables conform SAFIR reqs
@@ -54,6 +54,15 @@ def multi_Fmax(df,reffile,SW_removeIterations=True):
 	tISO=120 # [min]
 	## handling ##
 	tISO*=60 # [s] dimension change
+
+	## geometric imperfection handling ##
+		# note: (forced) interpretation of 'average eccentricity', 'out of plumbness', 'out of straightness'; cfr. SAFIR input
+		# average eccentricity handled through nodeline modification (*.tem)
+		# out of plumbness and out of straightness handled through nodal position modification (*.in)
+		# for out of plumbness, this must be revisited/confirmed
+	# Switch applied to allow to disable calculation - geomImperf does not check whether Imperf data is provided
+		# can be avoided by setting default values geomImperf input to zero within f[geomImperf]
+	if SW_geomImperf: df=geomImperf(df)
 
 	## *.in file realization ##
 	mod_inSAFIR_multifile(df,reffile,SW_newfolder)
@@ -104,8 +113,57 @@ def collectResults(df,sInfile,reffile):
 	outpath='\\'.join(reffile.split('\\')[0:-1])+'\\output'
 	Print_DataFrame([df],outpath,['out'])
 
+def geomImperf(df):
+	# takes df columns with imperfection input and add columns with nodal positions
+	# uses hardcoded (and implicit) info on number of nodes, nodal order, column height etc.
 
+	## hardcoded model parameters - column ##
+	l=4.0 # [m] column height
+	nNodes=21
+	nodeList=np.linspace(0,l,nNodes) # nodes along length of column
+	nodes=np.arange(nNodes) # note: does not correspond with SAFIR numbering: is natural Python numbering
+	nSim=len(df.index)
+	refPos=pd.DataFrame(0.,index=nodes,columns=['x','y']); refPos['y']=nodeList
 
+	## initialize output ##
+	xLabel=['x'+'{0}'.format(i).zfill(2) for i in nodes]
+	yLabel=['y'+'{0}'.format(i).zfill(2) for i in nodes]
+	Label=xLabel+yLabel
+	out=pd.DataFrame(index=df.index,columns=Label)
+
+	## out of straightness ##
+	s_oos=df['oos']; amplitude=s_oos.values.reshape(1,nSim)
+	angleList=np.linspace(0,np.pi,nNodes); sin_oos=np.sin(angleList)
+	sin=np.reshape(sin_oos,(nNodes,1))
+	xlist_oos=np.dot(sin,amplitude) # local lateral out of straightness
+	xlist_oos=pd.DataFrame(xlist_oos,columns=df.index,index=nodes)
+
+	## out of plumbness ##
+		# # depreciated : out of plumbness calculation neglecting out of straightness
+		# s_oop=df['oop']
+		# sin_oop=np.sin(s_oop)
+		# cos_oop=np.cos(s_oop)
+		# # reshape for matrix multiplication
+		# nodeL=np.reshape(nodeList,(nNodes,1))
+		# sin=sin_oop.values.reshape((1,nSim)); cos=cos_oop.values.reshape((1,nSim))
+		# Xlist_oop=np.dot(nodeL,sin); Ylist_oop=np.dot(nodeL,cos) # global positions considering out of plumbness
+	# new version
+	# effect out of plumbness, taking into account effect out of straightness
+	s_oop=df['oop'] 
+	sin_oop=np.sin(s_oop); cos_oop=np.cos(s_oop)
+	# apply rotation effect per realization
+	for sim in df.index:
+		startPos=deepcopy(refPos); startPos['x']+=xlist_oos[sim].values
+		rotation=np.array([[cos_oop[sim],-sin_oop[sim]],[sin_oop[sim],cos_oop[sim]]])
+		newPos=np.dot(startPos,rotation)
+		flatnewPos=newPos.flatten('F')
+		out.loc[sim]=flatnewPos
+
+	## append DataFrame
+	df=pd.concat([df,out],axis=1)
+
+	## return extended df of pointers and substitutes
+	return df
 
 #########################
 ## STAND ALONE - DEBUG ##
@@ -120,7 +178,7 @@ if __name__ == "__main__":
 	## SWITCH ##
 	SW_debug=False
 
-	I_test=3 # test indicator
+	I_test=5 # test indicator
 
 	###########
 	## SETUP ##
@@ -238,6 +296,69 @@ if __name__ == "__main__":
 
 			# run multi_Fmax
 			multi_Fmax(df,reffile)
+
+		elif I_test==4:
+
+			# out of plumbness; out of straightness
+			df=pd.DataFrame([[0.0015,0.0],[0.0,0.2]],columns=[0,1],index=['oop','oos']); df=df.transpose()
+
+			# geomImperf test
+			df=geomImperf(df)
+
+			# test
+			print(df)
+
+		elif I_test==5:
+
+			# combine I_test 3 and 4
+
+			## *.in file to be modified ##
+			reffile="C:\\Users\\rvcoile\\Documents\\Workers\\Imperfection\\reffileGeom.in"
+
+			## (stoch) variable realizations ##
+			# realizations of variables to be substituted
+			# for each column realization, Pmax will be calculated
+			fc=[25.2,30.7,30.] # [MPa] 20°C concrete compressive strength realization
+			fy=[512.0,420.6,500.] # [MPa] 20°C steel yield stress realization
+			nR=len(fc)
+			X=pd.DataFrame([fc,fy],index=['fc [MPa]','fy [MPa]'],columns=np.arange(nR))
+			X=X.T # transpose(X)
+
+			# test 1 df
+			df=X*10**6; df.columns=['fc20', 'fy20']
+
+			# add 'nodeline_Y'
+			df['nodeline_Y']=[0.0003,0.00002,0.0001]
+			df['oop']=[0.0015,0.002,0.0]
+			df['oos']=[0.004,0.003,0.0]
+
+			# run multi_Fmax
+			multi_Fmax(df,reffile)
+
+
+
+			# test 1 + nodeline adjustment ##
+
+			# ## *.in file to be modified ##
+			# reffile="C:\\Users\\rvcoile\\Documents\\Workers\\Imperfection\\reffileGeom.in"
+
+			# ## realizations pointers ##
+			# # copy test 3
+			# fc=[25.2,30.7,30.] # [MPa] 20°C concrete compressive strength realization
+			# fy=[512.0,420.6,500.] # [MPa] 20°C steel yield stress realization
+			# nR=len(fc)
+			# X=pd.DataFrame([fc,fy],index=['fc [MPa]','fy [MPa]'],columns=np.arange(nR))
+			# X=X.T # transpose(X)
+			# df=X*10**6; df.columns=['fc20', 'fy20']
+			# df['nodeline_Y']=[0.0003,0.00002,0.0001]
+			# # geom imperfection 'out of plumbness', 'out of straightness'
+			# df['oop']=[0.0015,0.003,0.0] # [rad] out of plumbness
+			# df['oos']=[0.004,0.001,0.0] # [m] out of straightness
+
+			# ## simulation run
+			# multi_Fmax(df,reffile)
+
+			
 
 	###########
 	## DEBUG ##
